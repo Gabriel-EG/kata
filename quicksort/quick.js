62ed936f2e522f3b1775a3d664576670dbe04551
Gabriel-EG <159055640+Gabriel-EG@users.noreply.github.com>
2024-02-14 12:04:14 -0300

    Tornando a função mais eficiente
    A função de ordenação terá no máximo a metade do tamanho da lista

--- quicksort/quick.js
+++ quicksort/quick.js
@@ -1,71 +1,82 @@
+const PARAMS = {
+    valorMaximo: 300,
+    quantidadeRegistros: 81
+};
+
 function atribuirHTML(id, textInner) {
     let element = document.getElementById(id);
     element.innerHTML = textInner;
 }
 
 function logLista(list, extraInfo) {
-    const text = JSON.stringify(list);
+    const text = JSON.stringify(list).replace(/,/g, ', ');
     console.log(extraInfo, text);
     return text;
 }
 
 function ordenar(list) {
-    let qtdTrocas = 0;
 
-    function comparar(item1, item2) {
+    function compararItem(item1, item2) {
         return item2 - item1;
     }
 
-    function ordenarItem(list, i, j) {
-        const diff = comparar(list[i], list[j]);
-        if (diff < 0) {
-            qtdTrocas++;
-            // I é maior
-            const temp = list[i];
-            list[i] = list[j];
-            list[j] = temp;
-            logLista(list, `troca ${j} por ${i}:`);
-        // } else if (diff > 0) {
-            // J é maior, nada a fazer
-        // } else {
-            // itens iguais
+    function compararReferencia(list, idx, refLimite, atualizarLimite) {
+        let diff = compararItem(list[idx], refLimite.valor);
+        if (atualizarLimite(diff)) {
+            refLimite.valor = list[idx];
+            refLimite.indice = idx;
         }
     }
 
-    for (let i = 0; i < list.length - 1; i++) {
-        // for (let j = i+1; j < list.length; j++) {
-        for (let j = list.length; j > i; j--) {
-            ordenarItem(list, i, j);
+    function trocar(list, idxOrigem, refLimite) {
+        if (idxOrigem != refLimite.indice) {
+            list[refLimite.indice] = list[idxOrigem];
+            list[idxOrigem] = refLimite.valor;
+            console.log(`troca ${refLimite.indice} por ${idxOrigem}:`);
         }
     }
-    return qtdTrocas;
+
+    let inicio = 0;
+    let fim = list.length - 1;
+    let passo = 1;
+    const limite = Math.floor(list.length / 2);
+    for (; inicio < fim; inicio++, fim--, passo++) {
+        const minimo = {valor: PARAMS.valorMaximo+1, indice: null};
+        const maximo = {valor: 0, indice: null};
+    
+        for (let idx = inicio; idx <= fim && inicio < fim; idx++) {
+            compararReferencia(list, idx, minimo, (diff) => diff > 0);
+            compararReferencia(list, idx, maximo, (diff) => diff < 0);
+        }
+        trocar(list, inicio, minimo);
+        if (maximo.indice == inicio) {
+            maximo.indice = minimo.indice;
+        }
+        trocar(list, fim, maximo);
+        if (passo < limite) {
+            logLista(list, `passo ${passo}:`);
+        }
+    }
+
+    return passo-1;
 }
 
 let lista = [];
 
 function init() {
     lista = [];
-    for (let i = 0; i < 20; i++) {
-        lista.push(Math.floor(Math.random() * 50) + 1);
+    for (let i = 0; i < PARAMS.quantidadeRegistros; i++) {
+        lista.push(Math.floor(Math.random() * PARAMS.valorMaximo) + 1);
     }
 
-    // const txOriginal = JSON.stringify(lista);
-    // console.log(txOriginal);
     const txOriginal = logLista(lista, 'inicial');
-
-    // let inicial = document.getElementById("inicial");
-    // inicial.innerHTML = txOriginal;
     atribuirHTML("inicial", txOriginal);
 }
 
 function execute() {
-    // const txResultado = JSON.stringify(lista.sort());
-    // console.log(txResultado);
     const qtdTrocas = ordenar(lista);
     const txResultado = logLista(lista, `resultado ${qtdTrocas}`);
 
-    // let resultado = document.getElementById("resultado");
-    // resultado.innerHTML = txResultado;
     atribuirHTML("resultado", txResultado);
 }
 
